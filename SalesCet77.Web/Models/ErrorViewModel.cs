c45de80c3b0a338b54ddf68900f960991ec7ef3c
Diogoepronto <bta.diogo@gmail.com>
2023-05-24 14:41:39 +0100

    Init project

---
+++ SalesCet77.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SalesCet77.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
